2255f5c068b78fa3de5c998aa9833389897f7947
asitkr <37330958+asitkr@users.noreply.github.com>
2022-07-10 10:06:56 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
+// JavaScript for Toggle menu
 
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
